11a40203410c906e5551003b8923cad43f658b8d
Malo Mn <malo.maisonneuve@univ-avignon.fr>
2023-12-18 15:55:32 +0100

    various notebooks & transcription recipe

---
+++ transcription/transcribe.sh
@@ -0,0 +1,16 @@
+#!/bin/bash --login 
+
+#SBATCH --job-name=wav2vec2phone
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --partition=gpu
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=16G
+#SBATCH --time=24:00:00
+#SBATCH --constraint='GPUArch_Pascal&GPURAM_Min_16GB&GPURAM_Max_32GB'
+
+echo "Activating environment wav2vec" > $1.txt
+conda activate wav2vec
+
+echo "Launching Wav2Vec2 fine-tuning" >> $1.txt
+python recipe.py wav2vec2_phoneme.yml >> $1.txt
